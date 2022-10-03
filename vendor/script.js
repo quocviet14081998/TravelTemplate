94f199b1ff50c94e24f1991d7e237eea8600a287
quocviet14081998 <quocviet14081998@gmail.com>
2022-10-03 14:59:51 +0700

    update form login

--- vendor/script.js
+++ vendor/script.js
@@ -15,3 +15,25 @@ tabs.forEach((tab, index) => {
     pane.classList.add("active");
   };
 });
+
+const signinBtn = document.querySelector(".signinBtn");
+const signupBtn = document.querySelector(".signupBtn");
+const formBx = document.querySelector(".formBx");
+const body = document.querySelector("body");
+const btnLogin = document.querySelector(".header--nav__item--contact__login");
+const formLogin = document.querySelector(".overlayLogin");
+const formClose = document.querySelector(".overlayLogin i");
+
+signupBtn.addEventListener("click", () => {
+  formBx.classList.add("active");
+});
+signinBtn.addEventListener("click", () => {
+  formBx.classList.remove("active");
+});
+
+btnLogin.addEventListener("click", () => {
+  formLogin.style.display = "flex";
+});
+formClose.addEventListener("click", () => {
+  formLogin.style.display = "none";
+});
